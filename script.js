933c57e8fb835d8a42e8aa3dbc85c89837f76a81
villehvo <89245411+villehvo@users.noreply.github.com>
2021-10-01 13:46:51 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function Schalom() {
+    alert("schalom");
+    document.getElementById("Rubrik1").innerHTML = "bruh";
+}
+function tid() {
+    alert("Tryck nu OK för att visa tiden");
+    document.getElementById("Tiden").innerHTML = Date();
+}
